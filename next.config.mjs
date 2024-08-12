350cd223ce8ac6b6d2680af63eb525271441de5d
ronitjadhav <ronitjadhav17@gmail.com>
2024-08-12 07:49:13 +0200

    Updates to config

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/digipin-openlayers",
-    output: "export",
-    reactStrictMode: true,
-    images: {
-        unoptimized: true,
-    },
-    env: {
-        NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
-        NEXT_PUBLIC_MAPBOX_LIGHT_STYLE_URL: process.env.NEXT_PUBLIC_MAPBOX_LIGHT_STYLE_URL,
-        NEXT_PUBLIC_MAPBOX_DARK_STYLE_URL: process.env.NEXT_PUBLIC_MAPBOX_DARK_STYLE_URL,
-    },
-    // This function is optional, but can be helpful if you have dynamic routes
-    // exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-    //   return {
-    //     '/': { page: '/' },
-    //     '/about': { page: '/about' },
-    //     // Add other routes as needed
-    //   }
-    // },
+    basePath : "/digipin-openlayers",
+    output : "export",
+    reactStrictMode : true,
 };
 
-module.exports = nextConfig;
+export default nextConfig;
