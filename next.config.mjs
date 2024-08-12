ea40fdf6f58d4d4bc065fa7728fcf9a41b13e163
ronitjadhav <ronitjadhav17@gmail.com>
2024-08-12 07:18:50 +0200

    Updates in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
     basePath : "/digipin-openlayers",
     output : "export",
     reactStrictMode : true,
+    env: {
+        NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN: process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN,
+        NEXT_PUBLIC_MAPBOX_LIGHT_STYLE_URL: process.env.NEXT_PUBLIC_MAPBOX_LIGHT_STYLE_URL,
+        NEXT_PUBLIC_MAPBOX_DARK_STYLE_URL: process.env.NEXT_PUBLIC_MAPBOX_DARK_STYLE_URL,
+    },
 };
 
 export default nextConfig;
